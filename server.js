ec9722660b988f4ab42d881b23a3a185018e9aa5
Isaac Aderonmu <aderonmuisaac@gmail.com>
2022-10-15 04:05:25 +0100

    [feat]: calculate request time for all requests

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const app = express();
 
 const PORT = 3000;
 
+// Add a middleware to handle logging within the application
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`[${req.method}], url=${req.url}, requestTimeMs=${delta}`)
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Server Listening on port ${PORT}...`);
 });
